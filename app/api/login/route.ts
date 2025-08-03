ebc0e1c52da9bfa0b2574ea953293a191a56b8b2
adri3nB <adrian.bazaldua@spot2.mx>
2025-08-03 16:05:15 -0600

    Enhance login API to use JWT for session management, replacing user ID with token in cookies and adding secure attributes for production environments.

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -4,6 +4,7 @@ import { db } from "@/src/index";
 import { eq } from "drizzle-orm";
 import { cookies } from "next/headers";
 import { compare } from "bcrypt"; // TODO: Cambiar a bcryptjs
+import { signJwt } from "@/lib/auth";
 
 export async function POST(req: Request) {
   const { email, password } = await req.json();
@@ -13,16 +14,20 @@ export async function POST(req: Request) {
     .from(usuario)
     .where(eq(usuario.email, email));
   const user = result[0];
+
   if (!user || user.password !== password) {
     return NextResponse.json(
       { error: "Credenciales inválidas" },
       { status: 401 }
     );
   }
+  const token = await signJwt({ id: user.id, email: user.email });
 
-  (await cookies()).set("session", user.id.toString(), {
+  (await cookies()).set("session", token, {
     path: "/",
     httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 60 * 60 * 24 * 7,
   });
 
   return NextResponse.json({ message: "Inicio de sesión exitoso" });
