bc047be448bd237a5c826317bd25f28538cb8784
adri3nB <adrian.bazaldua@spot2.mx>
2025-08-03 22:24:31 -0600

    Refactor UsersTable component to use client-side data fetching with pagination. Implement user retrieval via API and add pagination controls for navigation.

--- components/dashboard/users/users-table.tsx
+++ components/dashboard/users/users-table.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Table,
   TableBody,
@@ -16,51 +17,126 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Usuario } from "@/src/db/schema";
-import { getUsers } from "@/server/users";
 import UserForm from "./user-form";
 import { Pencil } from "lucide-react";
 import DeleteUserButton from "./delete-user-button";
+import { useEffect, useState } from "react";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 
-export default async function UsersTable() {
-  const users = await getUsers();
+export default function UsersTable() {
+  const [users, setUsers] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  const perPage = 10;
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch(
+        `/api/users?page=${page}&perPage=${perPage}`
+      );
+      const json = await response.json();
+      setUsers(json.data);
+      setTotalPages(json.totalPages);
+    };
+    fetchUsers();
+  }, [page]);
+
+  const handlePageChange = (page: number) => {
+    setPage(page);
+  };
+
   return (
-    <Table className="w-full table-auto">
-      <TableCaption>Lista de usuarios</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[15%]">ID</TableHead>
-          <TableHead className="w-[25%]">Nombre</TableHead>
-          <TableHead className="w-[25%]">Email</TableHead>
-          <TableHead className="w-[15%]">Rol</TableHead>
-          <TableHead className="w-[20%]">Acciones</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {users.map((user: Usuario) => (
-          <TableRow key={user.id}>
-            <TableCell>{user.id}</TableCell>
-            <TableCell>{user.nombre}</TableCell>
-            <TableCell className="font-medium">{user.email}</TableCell>
-            <TableCell>{user.rolId}</TableCell>
-            <TableCell>
-              <Dialog>
-                <DialogTrigger asChild>
-                  <Button variant="ghost" size="icon">
-                    <Pencil className="w-4 h-4 size-4 ml-2" />
-                  </Button>
-                </DialogTrigger>
-                <DialogContent>
-                  <DialogHeader>
-                    <DialogTitle>Editar Usuario</DialogTitle>
-                  </DialogHeader>
-                  <UserForm user={user} />
-                </DialogContent>
-              </Dialog>
-              <DeleteUserButton userId={user.id.toString()} />
-            </TableCell>
+    <div>
+      <Table className="w-full table-auto">
+        <TableCaption>Lista de usuarios</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[15%]">ID</TableHead>
+            <TableHead className="w-[25%]">Nombre</TableHead>
+            <TableHead className="w-[25%]">Email</TableHead>
+            <TableHead className="w-[15%]">Rol</TableHead>
+            <TableHead className="w-[20%]">Acciones</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {users.map((user: Usuario) => (
+            <TableRow key={user.id}>
+              <TableCell>{user.id}</TableCell>
+              <TableCell>{user.nombre}</TableCell>
+              <TableCell className="font-medium">{user.email}</TableCell>
+              <TableCell>{user.rolId}</TableCell>
+              <TableCell>
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button variant="ghost" size="icon">
+                      <Pencil className="w-4 h-4 size-4 ml-2" />
+                    </Button>
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>Editar Usuario</DialogTitle>
+                    </DialogHeader>
+                    <UserForm user={user} />
+                  </DialogContent>
+                </Dialog>
+                <DeleteUserButton userId={user.id.toString()} />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <Pagination className="mt-4">
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                if (page > 1) {
+                  handlePageChange(page - 1);
+                }
+              }}
+              className={`${
+                page === 1 ? "opacity-50 pointer-events-none" : ""
+              }`}
+              aria-disabled={page === 1}
+            />
+          </PaginationItem>
+          {Array.from({ length: totalPages }, (_, index) => (
+            <PaginationItem key={index}>
+              <PaginationLink
+                href="#"
+                isActive={page === index + 1}
+                onClick={() => handlePageChange(index + 1)}
+              >
+                {index + 1}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+          <PaginationItem>
+            <PaginationNext
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                if (page < totalPages) {
+                  handlePageChange(page + 1);
+                }
+              }}
+              className={`${
+                page === totalPages ? "opacity-50 pointer-events-none" : ""
+              }`}
+              aria-disabled={page === totalPages}
+            />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </div>
   );
 }
