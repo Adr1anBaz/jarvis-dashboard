7ea7b05708bf8f71636778590a60389f19886925
adri3nB <adrian.bazaldua@spot2.mx>
2025-08-03 20:10:37 -0600

    Update UsuariosPage to include UsersTable component and enhance layout with improved styling

--- app/dashboard/usuarios/page.tsx
+++ app/dashboard/usuarios/page.tsx
@@ -1,5 +1,12 @@
+import UsersTable from "@/components/dashboard/users/users-table";
+
 const UsuariosPage = () => {
-  return <div>UsuariosPage</div>;
+  return (
+    <div className="flex flex-col gap-4 max-w-7xl mx-auto p-4 md:p-24">
+      <h1 className="text-2xl font-bold mb-4">Usuarios</h1>
+      <UsersTable />
+    </div>
+  );
 };
 
 export default UsuariosPage;
